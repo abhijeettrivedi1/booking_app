9b48a49d03f0052c112358849ac309085c8386c5
abhijeettrivedi1 <127127641+abhijeettrivedi1@users.noreply.github.com>
2024-03-08 23:35:28 +0530

    Update App.jsx

--- book_app/src/App.jsx
+++ book_app/src/App.jsx
@@ -1,7 +1,7 @@
 
 import { Route,Routes } from 'react-router-dom'
 import './App.css'
-import IndexPage from './pages/indexpage'
+import IndexPage from './pages/IndexPage'
 import LoginPage from './pages/LoginPage'
 import Layout from './Layout'
 import RegisterPage from './pages/RegisterPage'
